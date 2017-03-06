0c31d958a930f03d691283a5a3446793e262ce14
Dario Pasquali <dario.pasquali@studio.unibo.it>
2017-03-06 13:10:17 +0100

    sistemare la Console e relativa GUI

--- SCOUT PROJECT/it.unibo.iot.pasquali.scout.Robot/robotTheory.pl
+++ SCOUT PROJECT/it.unibo.iot.pasquali.scout.Robot/robotTheory.pl
@@ -4,28 +4,40 @@ robotTheory.pl
 ===============================================================
 */
 
+%% SENTENCES EXECUTION ------------------------------------------------------
 
+myRunTheSentence(CURPLAN, Actor, sentence( GUARD, MOVE, EVENTS, PLANS ) ):-
+  	( GUARD = - G , !, retract(G),  ! ; GUARD, ! ),
+  	 myExecuteCmd(CURPLAN, Actor, MOVE, EVENTS, PLANS, RESULT).
+  
+myExecuteCmd(CURPLAN, Actor, move(receiveAndSolve,RECEIVETIME, MSGLIST, PAYLOADLIST, SOLVELIST, SOLVETIME), Events, Plans, RES ):-
+	Actor <- receiveMessageAndSolve( RECEIVETIME, MSGLIST, PAYLOADLIST, SOLVELIST, SOLVETIME).
+  	
+myExecuteCmd(CURPLAN, Actor, move(senseEvent,TIMEOUT,EVENTSLIST, PLANSLIST), Events, Plans, RES ):-
+	Actor <- mySenseEvent(TIMEOUT, EVENTSLIST, PLANSLIST).
+  	
+myExecuteCmd(CURPLAN, Actor, move(robotmove,CMD,SPEED,DURATION,ANGLE), Events, Plans, RES ):-
+ 	!,
+ 	mapCmdToMove(CMD,MOVE),
+	Actor <- execRobotMove(CURPLAN, MOVE, SPEED, ANGLE, DURATION, Events, Plans),
+	Actor <- updateMyPosition(MOVE).
 
-%% ROBOT CONFIG -------------------------------
-
-planFilename("scout.txt").
-planName(path).
-
-defaultSpeed(60).
-defaultDuration(500).
-
-%%-----------------------------------------------
-
-
-
-
+myExecuteCmd(CURPLAN, Actor,  move(print,ARG), Events, Plans, done(print) ):-
+	text_term(ARGS,ARG),
+	actorPrintln(  ARGS ).
+	
+	
+myExecuteCmd(CURPLAN, Actor, move(solve,GOAL,DURATION), Events, Plans, RES ):-
+	Actor <- solveSentence(sentence(true, GOAL, 0, '', '', '')) returns AAR,
+	AAR <- getResult returns RES.
+	
 
 /*
 ------------------------------------------------------------
 initialize
 ------------------------------------------------------------
 */
-initRobot  :-  
+initRobot :-  
 	actorPrintln("initRobot").
  
 :- initialization(initRobot).
